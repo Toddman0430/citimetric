d4d4ca2e41ea8eefa31ceaf497bc651b874e7f46
Todd Emory <ctemory7@gmail.com>
2012-09-21 00:14:55 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Citimetric::Application.config.secret_token = '33129c9ac580217551149528cdc4c676acb217f00d6009d20bbdbe9b748c9f2c7486bd8d8a2dccdc5a43f4de25d5ac6b03766816526d38b23b887c3388777ca3'
